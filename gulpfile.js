97df5000d3913ce7a00a3b36940b5081e00f93cb
Jordan anderson <jordan_anders@live.co.uk>
2017-09-23 09:32:53 +0100

    Adds patch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var webpack = require('webpack-stream');
 var del = require('del');
-var project = require('./package.json');
 var rename = require("gulp-rename");
+var gutil = require('gulp-util');
 
 var tsProject = ts.createProject('tsconfig.json');
-
+///////////////////////////////////////////////////////
+// CLEAN                                             //
+///////////////////////////////////////////////////////
 gulp.task('clean:all', function () {
     return del([
       'dist/**/*',
@@ -15,7 +17,21 @@ gulp.task('clean:all', function () {
     ]);
   });
 
-gulp.task('compile', ['clean:all'], function () {
+///////////////////////////////////////////////////////
+// PATCH                                             //
+///////////////////////////////////////////////////////
+
+gulp.task('patch', ['clean:all'], function () {
+    let projectAction = function(project) {
+            project.version = process.env.version || project.version
+            return project;
+        };
+
+    patchProject(projectAction)
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('compile', ['patch', 'clean:all'], function () {
 
     let tsResult = tsProject.src()
                             .pipe(tsProject());
@@ -30,11 +46,33 @@ gulp.task('copy', ['compile', 'webpack'], function() {
 
 gulp.task('webpack', ['compile'], function () {
 
-        gulp.src('./lib/loxium.js')
+      let project = require('./package.json');
+    
+        return gulp.src('./lib/loxium.js')
             .pipe(webpack(require('./webpack.config.js')))
             .pipe(uglify())
             .pipe(rename(`loxium-${project.version}.min.js`))
             .pipe(gulp.dest(`dist/`));
     });
 
-gulp.task('default', [ 'clean:all', 'compile', 'webpack', 'copy' ]);
+gulp.task('default', [ 'clean:all', 'patch', 'compile', 'webpack', 'copy' ]);
+
+function patchProject(action) {
+    let package = require('./package.json');
+
+    package = action(package);
+
+    let src = require('stream').Readable({ objectMode: true });
+
+    src._read = function () {
+      this.push(new gutil.File({
+        cwd: "",
+        base: "",
+        path: "package.json",
+        contents: new Buffer(JSON.stringify(package, null, 4))
+      }));
+      this.push(null);
+    };
+
+    return src
+  }
